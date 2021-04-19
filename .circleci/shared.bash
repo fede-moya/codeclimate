43f1223572b89eb75af1c89f2aeb9c87ff84fc51
Federico Moyá <federicomoyamartin@gmail.com>
2021-04-19 16:52:38 -0300

    wip

--- .circleci/shared.bash
+++ .circleci/shared.bash
@@ -2,6 +2,14 @@
 
 set -exuo pipefail
 
+VERSION=`cat VERSION`
+
+function install_hub() {
+    sudo apt update && sudo apt install -y git wget
+    url="$(wget -qO- https://api.github.com/repos/github/hub/releases/latest | tr '"' '\n' | grep '.*/download/.*/hub-linux-amd64-.*.tgz')"
+    wget -qO- "$url" | sudo tar -xzvf- -C /usr/bin --strip-components=2 --wildcards "*/bin/hub"
+}
+
 function login_to_dockerhub() {
   set +x
   docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
@@ -15,60 +23,34 @@ function login_to_rubygems() {
   printf -- "---\n:rubygems_api_key: ${GEM_HOST_API_KEY}\n" > $HOME/.gem/credentials
 }
 
-function install_hub() {
-    sudo apt update && sudo apt install -y git wget
-    url="$(wget -qO- https://api.github.com/repos/github/hub/releases/latest | tr '"' '\n' | grep '.*/download/.*/hub-linux-amd64-.*.tgz')"
-    wget -qO- "$url" | sudo tar -xzvf- -C /usr/bin --strip-components=2 --wildcards "*/bin/hub"
+function tag_version() {
+  ARTIFACTS_OUTPUT=artifacts.tar.gz
+  tar -c -f ${ARTIFACTS_OUTPUT} *.gem
+  GITHUB_TOKEN=${GITHUB_TOKEN} hub release create -a ${ARTIFACTS_OUTPUT} -m "v${VERSION}" ${VERSION}
+}
+
+function upload_docker_images() {
+  docker build --rm --tag codeclimate/codeclimate .
+  docker push codeclimate/codeclimate:latest
+  docker tag codeclimate/codeclimate "codeclimate/codeclimate:$VERSION"
+  docker push "codeclimate/codeclimate:$VERSION"
 }
 
 function publish?() {
   `git diff --quiet HEAD~ VERSION; echo $?` == 1
 }
 
-#function install_cc_test_reporter() {
-#  local version="${1:-latest}"
-#
-#  curl -L -o "/tmp/cc-test-reporter" "https://codeclimate.com/downloads/test-reporter/test-reporter-$version-linux-amd64"
-#  sudo mv /tmp/cc-test-reporter /usr/bin
-#  sudo chmod +x /usr/bin/cc-test-reporter
-#}
+function publish_new_version() {
+  set +x
+  # Build and push gem
+  gem build *.gemspec
+  gem push *.gem
 
-#function install_aws_cli() {
-#  sudo pip install --quiet --upgrade awscli
-#  aws configure set default.region us-east-1
-#  aws configure set default.s3.max_concurrent_requests 20
-#}
+  # Create gh tag
+  tag_version
 
+  # Push docker images
+  #upload_docker_images
 
-#function login_to_replicated() {
-#  set +x
-#  docker login --username "$REPLICATED_EMAIL" --password "$REPLICATED_PASSWORD" registry.replicated.com
-#  set -x
-#}
-
-#function login_to_gcr() {
-#  set +x
-#  docker login --username _json_key --password "$GCR_JSON_KEY" us.gcr.io
-#  set -x
-#}
-
-#function cp_test_coverage() {
-#  docker cp "test-workflow-${CIRCLE_WORKFLOW_ID}-node-${CIRCLE_NODE_INDEX}":/app/coverage coverage
-#
-#  cc-test-reporter format-coverage --input-type simplecov --output "./coverage/codeclimate.$CIRCLE_NODE_INDEX.json" --prefix "/app"
-#  mv coverage/.resultset.json "./coverage/.resultset.$CIRCLE_NODE_INDEX.json"
-#  printf "%s\n" "$CIRCLE_NODE_TOTAL" > ./coverage/node-total
-#}
-
-#function report_test_coverage() {
-#  cc-test-reporter sum-coverage coverage/codeclimate.*.json --parts "$(cat ./coverage/node-total)"
-#
-#  cc-test-reporter upload-coverage || echo "report coverage skipped"
-#}
-
-#function pull_images() {
-#  docker pull "$IMAGE_REF"
-#  docker pull codeclimate/api
-#  docker pull percona/percona-server-mongodb:3.2
-#  docker pull redis:2.8
-#}
+  set -x
+}
